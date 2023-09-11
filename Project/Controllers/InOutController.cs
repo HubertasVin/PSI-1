3ea79ed0c3b308f4794e9c52c809019ae3a91ca6
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2023-09-11 18:52:49 +0000

    Added model+view

--- Project/Controllers/InOutController.cs
+++ Project/Controllers/InOutController.cs
@@ -6,13 +6,12 @@ namespace Project.Controllers;
 
 public class InOutController : Controller
 {
-    private readonly ILogger<InOutController> _logger;
-    
-    public InOutController(ILogger<InOutController> logger)
+    // returns a localhost/InOut view
+    public IActionResult Index() 
     {
-        _logger = logger;
+        var InOutModel = new InOutModel { Message = "Testing123" };
+        return View(InOutModel); //passing model so that I could see content on web page
     }
-
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ Project/Models/InOutModel.cs
@@ -0,0 +1,12 @@
+namespace Project.Models;
+
+public class InOutModel
+{
+    public int Id { get; set; }
+    public required string Message { get; set; }
+    
+    public InOutModel()
+    {
+        
+    }
+}

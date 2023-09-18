239c42c7481668a2eae291ee3bff968cac53a809
aWhiteScreen <nemura.zygimantasa@gmail.com>
2023-09-18 14:23:20 +0300

    Added register/login, search bar and a simple database for conspects.

---
+++ Project/Data/Migrations/20230918101028_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialsetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Conspect",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conspect", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conspect");
+        }
+    }
+}
---
+++ Project/Models/Conspect.cs
@@ -0,0 +1,14 @@
+﻿namespace Project.Models
+{
+    public class Conspect
+    {
+        public Conspect()
+        {
+            
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string text { get; set; }
+    }
+}

f223c336bcc3626d643b7ada1453cb9ba8abfca9
HubertasVin <hubertas2003@gmail.com>
2023-09-22 17:25:30 +0300

    Initial testing with JSON files

--- Project/Models/InOutModel.cs
+++ Project/Models/InOutModel.cs
@@ -1,12 +1,37 @@
+using System.Text.Json;
+
 namespace Project.Models;
 
 public class InOutModel
 {
-    // public int Id { get; set; }
-    public string Message { get; set; }
-    
-    public InOutModel(string path)
+    public struct MessageData
     {
-        Message = File.ReadAllText(path);
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+    // public int Id { get; set; }
+    public MessageData[] Message { get; set; }
+    
+    public InOutModel(FileStream file)
+    {
+        JsonElement json = JsonDocument.Parse(file).RootElement;
+        if (json.ValueKind != JsonValueKind.Array) return;
+
+        List<MessageData> messages = new List<MessageData>();
+
+        foreach (JsonElement element in json.EnumerateArray())
+        {
+            if (element.TryGetProperty("name", out JsonElement name))
+            {
+                MessageData message = new MessageData();
+                message.Name = name.GetString();
+                if (element.TryGetProperty("message", out JsonElement text))
+                    message.Text = text.GetString();
+                messages.Add(message);
+                System.Console.WriteLine(message.Name?.ToString());
+            }
+        }
+
+        Message = messages.ToArray();
     }
 }

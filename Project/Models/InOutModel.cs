4be199e9be3c4b64b0d5d74e60c78d96ce1cf1f7
Žygimantas <nemura.zygimantasa@gmail.com>
2023-09-23 18:24:41 +0300

    Update InOutModel.cs(fix for windows machines)

--- Project/Models/InOutModel.cs
+++ Project/Models/InOutModel.cs
@@ -13,6 +13,7 @@ namespace Project.Models
             var chatHistory = new InOut.JSONParser(file);
             this.Messages = JSONParser.Messages;
             this.InputMessage = string.Empty; // Initialize InputMessage in the constructor
+            file.Close(); // Won't work on windows without this 
         }
 
         public void AddMessage(string name, string message, string path)

d44adec38ff85313698d7c37c5826974fe49fba6
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2023-09-29 13:39:56 +0000

    InOutController.cs cleanup.

--- Project/Controllers/InOutController.cs
+++ Project/Controllers/InOutController.cs
@@ -9,9 +9,6 @@ public class InOutController : Controller
     // returns a localhost/InOut view
     public IActionResult Index() 
     {
-        // using FileStream file = System.IO.File.OpenRead("src/SignalRData.json");
-        var InOutModel = new InOutModel();
-        // InOutModel.LoadAllMessages();
         return View(); //passing model so that I could see content on web page
     }
     public IActionResult Error()

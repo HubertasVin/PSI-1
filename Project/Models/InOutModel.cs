ba62a07d8927c7e3c9d0441abc8ca683c7e014f4
Olen Račkauskas <39692726+Sc0rpie@users.noreply.github.com>
2023-09-29 10:45:24 +0300

    Update InOutModel.cs
    Removed unnecessary code

--- Project/Models/InOutModel.cs
+++ Project/Models/InOutModel.cs
@@ -13,22 +13,10 @@ namespace Project.Models
             public DateTime Timestamp { get; set; }
         }
 
-        // public List<InOut.JSONParser.MessageData> Messages { get; set; }
-        // public string InputMessage { get; set; }
-
         public InOutModel()
         {
             var chatHistory = new JSONParser(path, chatMessages);
-            // Task.Run(() => this.GetAllMessages()).Wait();
-            // this.Messages = JSONParser.Messages;
-            // this.InputMessage = string.Empty; // Initialize InputMessage in the constructor
-            // file.Close(); // Won't work on windows without this 
         }
-
-        // public void LoadAllMessages() 
-        // {
-        //     Task.Run(() => GetAllMessages()).Wait();
-        // }
 
         public void AddMessage(string user, string message, DateTime date, string path, List<MessageData> Messages)
         {

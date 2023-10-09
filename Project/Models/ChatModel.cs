20b3b84f6fda32a2c168ddfd908858e239bcd36f
Olen <olen2003@gmail.com>
2023-10-09 13:10:34 +0300

    Removed MessagePriority enumerator from ChatModel.cs IComparable interface added to MessageData struct with the corresponding CompareTo method. Removed unused includes (using...)

--- Project/Models/ChatModel.cs
+++ Project/Models/ChatModel.cs
@@ -1,23 +1,21 @@
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Project.Hubs;
 namespace Project.Models
 {
     public class ChatModel : ChatHub
     {
-        // Task enum
-        public enum MessagePriority
-        {
-            Low,
-            Normal,
-            High
-        }
         // Task record struct
-        public record struct MessageData // struct used to store message data
+        public record struct MessageData : IComparable<MessageData> // struct used to store message data
         {
             // Task Properties 1
             public string User { get; set; }
             public string Message { get; set; }
             public DateTime Timestamp { get; set; }
-            public MessagePriority Priority { get; set; }
+
+            public int CompareTo(MessageData other) // CompareTo method used to sort messages by timestamp in descending order
+            {
+                return other.Timestamp.CompareTo(this.Timestamp);
+            }
         }
 
         public ChatModel() {}

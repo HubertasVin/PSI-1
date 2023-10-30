b820521b8d0ee2bf8cbee37b0f3d9958c3975621
Sc0rp <olen2003@gmail.com>
2023-10-30 13:08:25 +0200

    Add base model as well as topic model

---
+++ Project/Models/BaseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Project.Models;
+
+public class BaseModel
+{
+    public string id { get; init; } = Guid.NewGuid().ToString();
+}
---
+++ Project/Models/TopicModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Project.Models;
+
+public class TopicModel : BaseModel
+{
+    public string Name { get; set; }
+    public Subject Subject { get; init; }
+
+    public TopicModel()
+    {
+        
+    }
+
+    public TopicModel(string name, Subject subject)
+    {
+        Name = name;
+        Subject = subject;
+    }
+}

2fbafe7d9baf4f8d75dd1518700f616599ec0890
Sc0rp <olen2003@gmail.com>
2023-10-31 13:13:49 +0200

    [key] attribute added to BaseModel

--- Project/Models/BaseModel.cs
+++ Project/Models/BaseModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Project.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models;
 
 public class BaseModel
 {
+    [Key]
     public string id { get; init; } = Guid.NewGuid().ToString();
 }

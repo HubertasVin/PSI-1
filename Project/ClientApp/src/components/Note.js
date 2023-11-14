f11c3662f5b80ae2f6e106a71132c3a9d0151a53
Sc0rp <olen2003@gmail.com>
2023-11-14 15:13:39 +0200

    Dummy note.js added

---
+++ Project/ClientApp/src/components/Note.js
@@ -0,0 +1,44 @@
+﻿import { useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
+import {Comment} from "./Comment";
+
+export const Note = () => {
+    const {topicId} = useParams();
+    const [showComments, setShowComments] = useState(false);
+    const [topicName, setTopicName] = useState('');
+
+    useEffect(() => {
+        console.log(showComments)
+        fetch("https://localhost:7015/topic/get/"+topicId)
+            .then(response => response.json())
+            .then(data => {
+                setTopicName(data.name);
+            })
+    }, []);
+    
+    const OpenedComments = () => {
+        setShowComments(true);
+        console.log("Set show comments to true")
+    };
+
+    const ClosedComments = () => {
+        setShowComments(false);
+        console.log("Set show comments set to" + showComments)
+    };
+    
+    return(
+        <div>
+            <div className="comment-panel">
+                <p>{topicName}</p>
+                <button onClick={() => setShowComments(true)}>Comments</button>
+            </div>
+            <Comment
+                show={showComments}
+                onClose={ClosedComments}
+                topicId={topicId}
+            />
+        </div>
+      
+        
+    );
+}

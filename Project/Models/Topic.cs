d1f1db68af251d03a4bfc1c07c7386c58762c0cc
Olen <olen2003@gmail.com>
2023-11-20 15:20:43 +0200

    Empty constructor in topic added back

--- Project/Models/Topic.cs
+++ Project/Models/Topic.cs
@@ -5,6 +5,11 @@ public class Topic : BaseModel
     public string Name { get; set; }
     public Subject Subject { get; init; }
 
+    public Topic()
+    {
+        
+    }
+
     public Topic(string name, Subject subject)
     {
         Name = name;
